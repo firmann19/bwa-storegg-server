3b564085cc51a650e5b9246148261906faa0671c
firmann19 <firman19ramadhan@gmail.com>
2022-05-04 20:17:10 +0700

    upload github

---
+++ app/auth/router.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+const {signup, singin} = require('./controller');
+const multer = require('multer')
+const os = require('os')
+
+
+
+router.post('/signup', multer({dest : os.tmpdir()}).single('image') ,signup);
+router.post('/signin', singin);
+
+module.exports = router;
